ceb95117de5dc0b8a9b2aa4d0846feb9d1f05689
Word Eater <WordEaterNG@gmail.com>
2023-01-13 10:32:59 -0500

    Update Get-Strings.ps1

--- Get-Strings.ps1
+++ Get-Strings.ps1
@@ -21,31 +21,28 @@ function Get-Strings {
 	.PARAMETER MinimumLength
 	The shortest sequence of characters to be included in the search results.
 	
-	.PARAMETER StripNonPrintable
+	.PARAMETER ShowNonPrintable
 	By default, the function strips non-printable characters from the output. By enabling this, those characters will be included.
-	Not recommended when displaying output as STDOUT.
+	Not recommended when displaying output as STDOUT uhles combined with NonPrintablePlaceHolder.
 		
-	.PARAMETER PlaceHolder
+	.PARAMETER NonPrintablePlaceHolder
 	This value lets you substitute any other character for non-printable characters in the output.
-	If any value is specified, StripNonPrintable is automatically set to false.
+	If any value is specified, ShowNonPrintable is implicitly set to true.
 	
 	.EXAMPLE	
 	Get-Strings C:\WINDOWS\System32\notepad.exe
 	
 	.EXAMPLE
-	Get-Strings -FilePath C:\WINDOWS\System32\notepad.exe -MinimumLength 8 -Encoding Unicode
+	Get-Strings -FilePath C:\WINDOWS\System32\notepad.exe -MinimumLength -Encoding Unicode
 	
 	.EXAMPLE
-	Get-Strings C:\WINDOWS\System32\notepad.exe -StripNonPrintable 0
+	Get-Strings /usr/bin/attr -NonPrintablePlaceHolder '.' -Encoding Ascii -Verbose
 	
 	.EXAMPLE
 	Get-Strings C:\WINDOWS\System32\notepad.exe -PlaceHolder '.'
 	
 	.EXAMPLE
-	Get-ChildItem C:\WINDOWS\*.dll -Recurse | ForEach-Object { Get-Strings $_ -MinimumLength 12 }
-	
-	.EXAMPLE
-	Get-Strings /usr/bin/mousepad -MinimumLength 8 -PlaceHolder '.' 
+	Get-Strings /usr/bin/mousepad -MinimumLength 8 -ShowNonPrintable | Out-File .\mousepad-strings.txt
 	
 	.NOTES
 	Written by Word Eater (WordEaterNG@gmail.com)
@@ -60,60 +57,74 @@ function Get-Strings {
 	It also takes being called out when it makes an error much better than some real people.
 	
 	It has been tested on Windows 10, Windows Server 2019, and Kali Linux 2022.4
-	
-	.LINK
-	https://github.com/wordeater/Get-Strings
 	#>
 	
 	[CmdletBinding()]
 	Param
-    (
-        [Parameter(Position = 1, Mandatory = $True, ValueFromPipelineByPropertyName = $True)]
-        [ValidateNotNullOrEmpty()]
-        [ValidateScript({Test-Path $_ -PathType 'Leaf'})]
-        [String[]]
-        [Alias('PSPath')]
-        $FilePath,
+	(
+        	[Parameter(Position = 0, Mandatory = $True, ValueFromPipelineByPropertyName = $True)]
+	        [ValidateNotNullOrEmpty()]
+        	[ValidateScript({Test-Path $_ -PathType 'Leaf'})]
+        	[String[]]
+        	[Alias('PSPath')]
+        	$FilePath,
 
-        [ValidateSet('Default','Ascii','Unicode')]
-        [String]
-        $Encoding = 'Default',
+        	[ValidateSet('Default','Ascii','Unicode')]
+        	[String]
+        	$Encoding = 'Default',
 
-        [UInt32][ValidateRange(0, [Int32]::MaxValue)]
-        $MinimumLength = 4,
+        	[UInt32][ValidateRange(0, [Int32]::MaxValue)]
+        	$MinimumLength = 4,
 		
-	[System.Boolean]
-	$StripNonPrintable=$true,
+		[Switch]
+		$ShowNonPrintable,
 		
-	[String]
-	$PlaceHolder=''
-    )
+		[String]
+		$NonPrintablePlaceHolder=''
+	)
 	
-	Begin {
+	Begin {	
+		# creating helper function
+		function Get-TimeStamp {
+			$TimeStamp = "[{0:yyyy-MM-dd} {0:HH:mm:ss}]" -f (Get-Date)
+			return $TimeStamp
+		}
+		$StartDateTime = Get-Date
+		
 		# Don't strip non-printable characters if a placeholder value is specified
-		if ( $PlaceHolder -ne '' ) { $StripNonPrintable = $false; }
+		if ( $NonPrintablePlaceHolder -ne '' ) { $ShowNonPrintable = $true; }
+		
+		$(Get-Timestamp) + "`tFile to check: " + (Get-ChildItem $FilePath).FullName | Write-Verbose
+		$(Get-Timestamp) + "`tMininum string length: " + $MinimumLength | Write-Verbose
+		$(Get-Timestamp) + "`tEncoding: " + $Encoding | Write-Verbose
+		$(Get-Timestamp) + "`tShow non-printable characters: " + $ShowNonPrintable | Write-Verbose
+		$(Get-Timestamp) + "`tNon-printable placeholder character: " + $NonPrintablePlaceHolder | Write-Verbose
 	}
 
 	Process {	
 		# Search for strings of printable characters of MinimumLength or longer, default 4
 		if ($Encoding -eq 'Ascii' -or $Encoding -eq 'Default') {
 			$strings = Select-String $FilePath -Pattern "[\x20-\x7E]{$MinimumLength,}"
-			if ( $PlaceHolder -ne '' ) { $strings = $strings | % { $_ -replace "[^\x20-\x7E]+",$PlaceHolder } }
-			if ( $StripNonPrintable ) { $strings = $strings | % { $_ -replace "[^\x20-\x7E]+" } }
-			Write-Verbose '---------- BEGIN ASCII STRINGS ----------'
+			if ( $NonPrintablePlaceHolder -ne '' ) { $strings = $strings | % { $_ -replace "[^\x20-\x7E]+",$NonPrintablePlaceHolder } }
+			if ( $ShowNonPrintable -eq $false ) { $strings = $strings | % { $_ -replace "[^\x20-\x7E]+" } }
+			$(Get-Timestamp) + "`t" + '---------- BEGIN ASCII STRINGS ----------' | Write-Verbose
 			$strings
-			Write-Verbose '---------- END ASCII STRINGS ----------'
+			$(Get-Timestamp) + "`t" + '---------- END ASCII STRINGS ----------' | Write-Verbose
 		}
 		if ($Encoding -eq 'Unicode' -or $Encoding -eq 'Default') {		
 			$strings = Select-String $FilePath -Pattern "[\u0020-\u007E]{$MinimumLength,}"
 			#$strings = Select-String $FilePath -Pattern "\p{L}{$MinimumLength,}"
-			if ( $PlaceHolder -ne '' ) { $strings = $strings | % { $_ -replace "[^\x20-\x7E]+",$PlaceHolder } }
-			if ( $StripNonPrintable ) { $strings = $strings | % { $_ -replace "[^\x20-\x7E]+" } }
-			Write-Verbose '---------- BEGIN UNICODE STRINGS ----------'
+			if ( $NonPrintablePlaceHolder -ne '' ) { $strings = $strings | % { $_ -replace "[^\x20-\x7E]+",$NonPrintablePlaceHolder } }
+			if ( $ShowNonPrintable -eq $false  ) { $strings = $strings | % { $_ -replace "[^\x20-\x7E]+" } }
+			$(Get-Timestamp) + "`t" + '---------- BEGIN UNICODE STRINGS ----------' | Write-Verbose
 			$strings
-			Write-Verbose '---------- END UNICODE STRINGS ----------'
+			$(Get-Timestamp) + "`t" + '---------- END UNICODE STRINGS ----------' | Write-Verbose
 		}		
 	}
 	
-	End { }
+	End {
+		$EndDateTime = Get-Date
+		$Duration = New-TimeSpan -Start $StartDateTime -End $EndDateTime
+		$(Get-Timestamp) + "`t" + "Execution took $Duration" | Write-Verbose
+	}
 }
